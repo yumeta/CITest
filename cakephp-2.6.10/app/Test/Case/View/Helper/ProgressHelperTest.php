2c67eb521f8236fbf3cb5b52af7ed35d615c95b3
yumeta <grooo_umeta@grooo-umeta-no-MacBook-Pro.local>
2015-09-14 09:49:58 +0900

    最初

---
+++ cakephp-2.6.10/app/Test/Case/View/Helper/ProgressHelperTest.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('Controller', 'Controller');
+App::uses('View', 'View');
+App::uses('ProgressHelper', 'View/Helper');
+
+class ProgressHelperTest extends CakeTestCase {
+    public function setUp() {
+
+    }
+
+    public function testBar() {
+
+    }
+}
+

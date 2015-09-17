80e4cebdbb994d49d24e9a6b7a9bce59f17ec1d3
yumeta <grooo_umeta@grooo-umeta-no-MacBook-Pro.local>
2015-09-17 09:03:53 +0900

    codeshipテスト

--- SampleTest.php
+++ SampleTest.php
@@ -3,6 +3,6 @@ class SampleTest extends PHPUnit_Framework_TestCase
 {
     public function testハローワールド()
     {
-        $this->assertEquals('a', 'a');
+        $this->assertEquals('a', 'b');
     }
 }
